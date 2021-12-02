fedc30817dab4cec8dae38ba33d163f19536fefb
porkyspar4 <erickmendieta011300@gmail.com>
2021-12-01 21:28:43 -0600

    printPDF

--- reportes/biblioteca.php
+++ reportes/biblioteca.php
@@ -1,2 +1,41 @@
 <?php
+require 'imports/dbConnection.php';
+require 'queries/comboBibliotecas.php';
+$query = "SELECT * FROM biblioteca ORDER BY id_biblioteca ASC";
+$rows = mysqli_query($connection, $query);
+?>
+
+
+
+
+<h1>Reporte Por Biblioteca</h1>
+
+
+<div>
+    <form method="POST" action="reportes/printPDFbiblioteca.php">
+        <table >
+            <tr>
+                <td class="input_cell"><?php echo 'Elegir Biblioteca:'; ?></td>
+                <td >
+                    <select name="id_biblioteca" id ="id_biblioteca" >
+                        <?php
+                        while ($row = mysqli_fetch_array($rows)) {
+                            ?>
+                            <option value="<?php echo $row['id_biblioteca']; ?>" 
+                            <?PHP
+                            if (isset($_POST['id_biblioteca']) && isset($index)) {
+                                echo $row['id_biblioteca'] == $_POST['id_biblioteca'][$index] ? "selected" : "";
+                            }
+                            ?>>
+                            <?php echo $row['nom_biblioteca']; ?></option>
+
+                            <?php
+                        }
+                        ?>
+                    </select></td>
+            </tr>     
+        </table>
+        <input class="reporteBiblioteca" type="submit" name="registrar" value="Generar Reporte"><br>                    
+    </form>
+</div>
 
