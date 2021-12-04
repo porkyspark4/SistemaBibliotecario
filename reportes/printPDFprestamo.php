a8c10aeeab9a8bea6089ba9316f42bde0bb088af
porkyspar4 <erickmendieta011300@gmail.com>
2021-12-03 21:35:07 -0600

    PDFprestamo corregido

--- reportes/printPDFprestamo.php
+++ reportes/printPDFprestamo.php
@@ -9,18 +9,18 @@ class PDF_nueva extends FPDF {
 //Cabecera de página
     function Header() {
         $this->SetFont('Arial', 'B', 24);
-        $this->Cell(100, 60, 'Comprobante Prestamo', 0, 0, 'C');
+        $this->Cell(180, 20, 'Comprobante Prestamo', 0, 0, 'C');
 
         $this->SetFont('Arial', 'B', 18);
         $this->Ln();
-        $this->Ln(40);
+        $this->Ln(5);
 //        $this->Ln();
     }
 
     function Footer() {
         $this->SetY(-10);
         $this->SetFont('Arial', 'I', 8);
-        $this->Cell(0, 10, 'Pagina ' . $this->PageNo() . '  comprobante_prestamo  ' . date("d/m/Y") . 0, 0, 'C');
+        $this->Cell(0, 10, '  Comprobante_prestamo  ' . date("d/m/Y") . 0, 0, 'C');
     }
 
 }
@@ -40,33 +40,36 @@ $pdf->setTitle("ComprobantePrestamo" . ' ' . date("d/m/Y"));
 
 
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->setX(40);
+$pdf->setX(50);
 $pdf->Cell(40, 7, "Fecha de prestamo", 0, 0, 'C');
 $pdf->Cell(60, 7, "Fecha limite de devolucion", 0, 1, 'C');
 
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->setX(40);
+$pdf->setX(50);
 $pdf->Cell(40, 7, utf8_decode($fecha_prestamo), 0, 0, 'C');
 $pdf->Cell(60, 7, utf8_decode($fecha_lim_devolucion), 0, 1, 'C');
 
 $pdf->Ln();
-
+$pdf->setX(30);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(40, 7, "No. de prestamo", 0, 0, 'C');
+$pdf->Cell(40, 7, "No. de prestamo :", 0, 0, 'L');
 $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(70, 7, utf8_decode($id_enc_prestamo), 0, 1, 'L');
 
+$pdf->setX(30);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(40, 7, "Empleado", 0, 0, 'C');
+$pdf->Cell(40, 7, "Empleado :", 0, 0, 'L');
 $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(70, 7, utf8_decode($empleado), 0, 1, 'L');
 
+$pdf->setX(30);
 $pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(40, 7, "Usuario", 0, 0, 'C');
+$pdf->Cell(40, 7, "Usuario :", 0, 0, 'L');
 $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(70, 7, utf8_decode($usuario), 0, 1, 'L');
 
 $pdf->Ln();
+$pdf->setX(30);
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(30, 7, "Clave", 1, 0, 'C');
 $pdf->Cell(70, 7, "Titulo", 1, 0, 'C');
@@ -83,7 +86,7 @@ while ($row = mysqli_fetch_array($rows)) {
     $id_libro_biblioteca = $row['id_libro_biblioteca'];
     $titulo = $row['titulo'];
     $nom_biblioteca = $row['nom_biblioteca'];
-
+    $pdf->setX(30);
     $pdf->Cell(30, 7, utf8_decode($id_libro_biblioteca), 1, 0, 'R');
     $pdf->Cell(70, 7, utf8_decode($titulo), 1, 0, 'C');
     $pdf->Cell(50, 7, utf8_decode($nom_biblioteca), 1, 1, 'C');
